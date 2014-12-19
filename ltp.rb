960597d2105dc20ff2d05ad2fb7a23a359ed04a2
Christopher Wood <christopherwood07@gmail.com>
2014-12-19 10:03:59 -0800

    Some updates

---
+++ ltp.rb
@@ -0,0 +1,36 @@
+#
+# ltp.rb
+# 
+# Convert a TeX file into plaintext so that it can be fed into popular
+# style, spelling, grammar, etc. checkers. 
+#
+
+require 'optparse'
+
+class LTPOptionParser
+	options = {}
+
+	def self.parse(args)
+		OptionParser.new do |opts|
+			opts.banner = "Usage: ltp.rb [options]"
+
+			opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+				options[:verbose] = v
+			end
+
+			# TODO: add other options as needed
+
+			opts.on_tail("--version", "Show version") do
+				puts "0.1"
+				exit
+			end
+
+		end.parse!
+		return options
+	end
+end
+
+options = LTPOptionParser.parse(ARGV)
+puts options[:verbose]
+
+# handle = File.open(options["filename"], "r")
